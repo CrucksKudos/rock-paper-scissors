ae71a613236e5bb7543e853a1ec8e93cbcaa624e
CrucksKudos <29085362+CrucksKudos@users.noreply.github.com>
2025-01-28 22:02:47 -0300

    Add getComputerChoice function on script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello, World')
+function getComputerChoice() {
+    let n = Math.floor(Math.random() * 3 + 1);
+    
+    if (n == 1) {
+        return 'Rock'
+    }
+    else if (n == 2) {
+        return 'Paper'
+    }
+    else {
+        return 'Scissors'
+    }
+}
+
+console.log(getComputerChoice())
